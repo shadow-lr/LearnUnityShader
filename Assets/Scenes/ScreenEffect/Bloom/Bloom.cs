68ecfbcdafd7549a21e9b9daaf118ee71531ce2f
shadow-lr <shaodow_lr@163.com>
2021-07-06 09:32:38 +0800

    Add edge detection shader

---
+++ Assets/Scenes/ScreenEffect/Bloom/Bloom.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*
+ * 根据一个阈值提取出图像中的较亮区域，把它们存储在一张渲染纹理中，再利用高斯模糊对这张渲染纹理进行模糊处理，模拟光线扩散的效果，最后再将其和原图像进行混合，得到最终效果
+ */
+public class Bloom : PostEffectsBase
+{
+    public Shader bloomShader;
+    public Material bloomMaterial = null;
+
+    public Material material
+    {
+        get
+        {
+            bloomMaterial = CheckShaderAndCreateMaterial(bloomShader, bloomMaterial);
+            return bloomMaterial;
+        }
+    }
+
+    [Header("迭代次数")] [Range(0, 4)] public int iterations = 3;
+    [Header("曝光强度")] [Range(0.2f, 3.0f)] public float blurSpread = 0.6f;
+
+    [Header("采样数")] [Range(1, 8)] public int downSample = 2;
+
+    [Header("像素的亮度值")] [Range(0.0f, 4.0f)] public float luminanceThreshold = 0.6f;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material)
+        {
+            // material.SetFloat("_EdgeOnly", edgesOnly);
+            // material.SetColor("_EdgeColor", edgeColor);
+            // material.SetColor("_BackgroundColor", backgroundColor);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
---
+++ Assets/Scenes/ScreenEffect/EdgeDetection/EdgeDetection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EdgeDetection : PostEffectsBase
+{
+    public Shader edgeDetectionShader;
+    public Material edgeDetectionMaterial = null;
+
+    public Material material
+    {
+        get
+        {
+            edgeDetectionMaterial = CheckShaderAndCreateMaterial(edgeDetectionShader, edgeDetectionMaterial);
+            return edgeDetectionMaterial;
+        }
+    }
+
+    [Header("边缘线强度")] [Range(0.0f, 1.0f)] public float edgesOnly = 0.0f;
+    [Header("描边颜色")] public Color edgeColor = Color.black;
+    [Header("背景颜色")] public Color backgroundColor = Color.white;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        if (material)
+        {
+            material.SetFloat("_EdgeOnly", edgesOnly);
+            material.SetColor("_EdgeColor", edgeColor);
+            material.SetColor("_BackgroundColor", backgroundColor);
+
+            Graphics.Blit(source, destination, material);
+        }
+        else
+        {
+            Graphics.Blit(source, destination);
+        }
+    }
+}
